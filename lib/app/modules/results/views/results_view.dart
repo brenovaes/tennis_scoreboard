6031949889910a613aa4dd4efbe14dea594a9005
Breno Novaes <brenonovaes@hotmail.com>
2022-04-14 16:31:33 -0300

    Tratando lista vazia de partidas

--- lib/app/modules/results/views/results_view.dart
+++ lib/app/modules/results/views/results_view.dart
@@ -1,8 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:collection/collection.dart';
 
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:tennis_scoreboard/app/routes/app_pages.dart';
+import 'package:tennis_scoreboard/app/ui/widgets/custom_elevated_button.dart';
 
 import '../controllers/results_controller.dart';
 
@@ -16,73 +20,106 @@ class ResultsView extends GetView<ResultsController> {
         title: const Text('Partidas passadas'),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Obx(
-          () => Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ExpansionPanelList(
-              expansionCallback: (int index, bool isExpanded) => controller
-                  .isExpanded
-                  .value = controller.isExpanded.value == index ? null : index,
-              children: controller.matchesList
-                  .mapIndexed<ExpansionPanel>((index, item) {
-                return ExpansionPanel(
-                  canTapOnHeader: true,
-                  isExpanded: controller.isExpanded.value == index,
-                  headerBuilder: (BuildContext context, bool isExpanded) {
-                    return ListTile(
-                      title: Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Flexible(
-                              child: Text(
-                                item.teams[0].length == 1
-                                    ? item.teams[0].first
-                                    : '${item.teams[0].first} & ${item.teams[0].last}',
-                                softWrap: true,
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            Flexible(
-                              child: Text(
-                                '${item.result.first.toString()} : ${item.result.last.toString()}',
-                              ),
-                            ),
-                            Flexible(
-                              child: Text(
-                                item.teams[1].length == 1
-                                    ? item.teams[1].first
-                                    : '${item.teams[1].first} & ${item.teams[1].last}',
-                                softWrap: true,
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ],
+      body: Obx(
+        () => controller.matchesList.isEmpty
+            ? Padding(
+                padding: const EdgeInsets.all(16),
+                child: SizedBox(
+                  width: Get.width,
+                  height: Get.height,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Você ainda não gravou nenhuma partida. Que tal começar?',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
                         ),
                       ),
-                    );
-                  },
-                  body: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          'Data da partida: ${DateFormat('dd/MM/yyyy, HH:mm').format(item.date)}',
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        width: Get.width,
+                        child: CustomElevatedButton(
+                          label: 'Configurar partida',
+                          onPressed: () => Get.toNamed(Routes.START),
+                          backgroundColor: const Color(0xFFFCEC63),
                         ),
-                        Text(
-                          'Duração: ${item.matchDuration}',
-                        ),
-                        Text('Sets: ${_buildSetsList(item.sets)}'),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                );
-              }).toList(),
-            ),
-          ),
-        ),
+                ),
+              )
+            : SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ExpansionPanelList(
+                    expansionCallback: (int index, bool isExpanded) =>
+                        controller.isExpanded.value =
+                            controller.isExpanded.value == index ? null : index,
+                    children: controller.matchesList
+                        .mapIndexed<ExpansionPanel>((index, item) {
+                      return ExpansionPanel(
+                        canTapOnHeader: true,
+                        isExpanded: controller.isExpanded.value == index,
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            title: Center(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      item.teams[0].length == 1
+                                          ? item.teams[0].first
+                                          : '${item.teams[0].first} & ${item.teams[0].last}',
+                                      softWrap: true,
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  Flexible(
+                                    child: Text(
+                                      '${item.result.first.toString()} : ${item.result.last.toString()}',
+                                    ),
+                                  ),
+                                  Flexible(
+                                    child: Text(
+                                      item.teams[1].length == 1
+                                          ? item.teams[1].first
+                                          : '${item.teams[1].first} & ${item.teams[1].last}',
+                                      softWrap: true,
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                        body: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Text(
+                                'Data da partida: ${DateFormat('dd/MM/yyyy, HH:mm').format(item.date)}',
+                              ),
+                              Text(
+                                'Duração: ${item.matchDuration}',
+                              ),
+                              Text('Sets: ${_buildSetsList(item.sets)}'),
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
       ),
     );
   }
